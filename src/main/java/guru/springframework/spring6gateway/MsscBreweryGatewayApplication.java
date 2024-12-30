e6af04e70e7ceb6c1f4a00080c2722cf1430bd16
cagla.boynuegri <cagla.boynuegri@scalefocus.com>
2024-12-30 15:18:26 +0300

    refactored package names for spring cloud project

--- src/main/java/guru/springframework/spring6gateway/MsscBreweryGatewayApplication.java
+++ src/main/java/guru/springframework/spring6gateway/MsscBreweryGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring6GatewayApplication {
+public class MsscBreweryGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6GatewayApplication.class, args);
+        SpringApplication.run(MsscBreweryGatewayApplication.class, args);
     }
 
 }
